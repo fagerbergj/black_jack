b0d4af7733e5aa9f88b5d1d3db46aafdee710fea
Jason Fagerberg <fagerbergj1@wit.edu>
2018-03-07 18:42:39 -0500

    reworked file structure

---
+++ src/black_jack.py
@@ -0,0 +1,10 @@
+from dealer import Dealer
+from player import Player
+from deck import Deck
+
+def main():
+    pass
+    
+
+if __name__ == "__main__":
+    main()
---
+++ src/game.py
@@ -0,0 +1,8 @@
+from dealer import Dealer
+from player import Player
+from deck import Deck
+
+class Game():
+    def __init__(self):
+        self.dealer = Dealer(input("Dealer Name: "), input("House Limit: "), Deck(), [])
+        self.player = Player(input("Player Name: "), input("Player Limit: "), [])
