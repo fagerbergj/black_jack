cd4f0766cd1d0f2145c679b6c2b618a63c4c4ac4
Jason Fagerberg <fagerbergj1@wit.edu>
2018-04-09 20:42:17 -0400

    fixed card value map to make more sense when creating a card

--- src/deck.py
+++ src/deck.py
@@ -32,5 +32,5 @@ class Deck():
         Card(11,"Hearts"), Card(11,"Diamonds"), Card(11,"Spades"), Card(11,"Clubs"),
         Card(12,"Hearts"), Card(12,"Diamonds"), Card(12,"Spades"), Card(12,"Clubs"),
         Card(13,"Hearts"), Card(13,"Diamonds"), Card(13,"Spades"), Card(13,"Clubs"),
-        Card(1,"Hearts"), Card(1,"Diamonds"), Card(1,"Spades"), Card(1,"Clubs")] * 4
+        Card((1,11),"Hearts"), Card((1,11),"Diamonds"), Card((1,11),"Spades"), Card((1,11),"Clubs")] * 4
         self.shuffle()
